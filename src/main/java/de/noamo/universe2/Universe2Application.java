35a31fcf634f9df9d08adb3518dcaddad158c7fc
Noah Moritz <30773809+NoahMoritz@users.noreply.github.com>
2020-11-23 09:40:21 +0100

    Initial Commit 1

---
+++ src/main/java/de/noamo/universe2/Universe2Application.java
@@ -0,0 +1,13 @@
+package de.noamo.universe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Universe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Universe2Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/noamo/universe2/Universe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.noamo.universe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Universe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
